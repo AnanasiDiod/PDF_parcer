fe8700c9feed124c20222ad01c71ed44ae2d61b0
AnanasiDiod <61230551+AnanasiDiod@users.noreply.github.com>
2023-02-12 22:06:42 +0300

    Update main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+import tabula
+
+# Read pdf into list of DataFrame
+# dfs = tabula.read_pdf("test.pdf", pages='all')
+
+# Read remote pdf into list of DataFrame
+dfs2 = tabula.read_pdf(
+    "https://github.com/tabulapdf/tabula-java/raw/master/src/test/resources/technology/tabula/arabic.pdf")
+
+# convert PDF into CSV file
+tabula.convert_into("D:/Documents/Mydocks/Работа/Jiner/test.pdf", "D:/Documents/Mydocks/Работа/Jiner/output.csv",
+                    output_format="csv", pages='all')
+
+# convert all PDFs in a directory
+tabula.convert_into_by_batch(
+    "input_directory", output_format='csv', pages='all')
